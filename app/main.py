fa09588fc4baba49bcf7db7bcde9c567069793f3
Jarmpad <jarmpad361@g.educaand.es>
2025-05-12 16:18:46 +0200

    Punto de confirmación 2

--- app/main.py
+++ app/main.py
@@ -4,9 +4,19 @@ from app.database import create_db_and_tables
 
 app = FastAPI()
 
+
 @app.on_event("startup")
 def on_startup():
     create_db_and_tables()
 
-app.include_router( tasks.router )
 
+app.include_router(tasks.router)
+
+
+app.include_router(tasks.router)
+
+
+# Código que se añade
+@app.get("/version")
+def version():
+    return {"message": "Armario Padilla, Juan Antonio - v0"}
